eb0cde949190cd60eadadeff1a0ceae1b112b7d4
gerbias <gerbias@yahoo.com>
2015-01-26 20:29:47 -0800

    Switched all terrain and objects to the same enum: this is useful as it matches the way that Tiled numbers the tile types.

--- include/defs.h
+++ include/defs.h
@@ -4,21 +4,31 @@
 enum tiles_t
 {
 m_exit = 1,
-m_pressure = 2,
-m_floor = 3,
-m_wall = 4,
-m_barrier = 128,
-m_conveyorn = 6,
-m_conveyors = 7,
-m_conveyorw = 8,
-m_conveyore = 5,
-m_any,
-m_error
+m_pressure,
+m_floor,
+m_wall,
+m_conveyore,
+m_conveyorn,
+m_conveyors,
+m_conveyorw,
+m_barrier,
+OBJ_BLOCK,
+OBJ_HEAVY_BLOCK,
+OBJ_ICE_BLOCK,
+OBJ_PLAYER,
+OBJ_ICE_ELEMENT_E,
+OBJ_ICE_ELEMENT_N,
+OBJ_ICE_ELEMENT_S,
+OBJ_ICE_ELEMENT_W,
+m_sender1,
+m_receiver1,
+
+m_any = 255,
+m_error = 0
 };
 enum connectors_t
 {
-	m_sender1 = 17,
-	m_receiver1 = 18
+
 };
 #define MAP_SIZE 48
 enum directions_t
@@ -31,14 +41,7 @@ enum directions_t
 };
 enum objects_t
 {
-	OBJ_BLOCK = 9,
-	OBJ_HEAVY_BLOCK = 10,
-	OBJ_ICE_BLOCK = 11,
-	OBJ_PLAYER = 12,
-	OBJ_ICE_ELEMENT_E = 13,
-	OBJ_ICE_ELEMENT_N = 14,
-	OBJ_ICE_ELEMENT_S = 15,
-	OBJ_ICE_ELEMENT_W = 16
+
 };
 #define MAX_LEVEL 5
 #define NUM_FRAMES 3
