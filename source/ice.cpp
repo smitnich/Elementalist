6ae5e513d4ed691adca0b396c5c0ba0fdf4544be
gerbias <gerbias@yahoo.com>
2015-01-30 17:54:37 -0800

    Adding ice source files.

---
+++ source/ice.cpp
@@ -0,0 +1,21 @@
+#include "SDLFiles.h"
+#include "terrain.h"
+#include "sprites.h"
+IceFloor::IceFloor()
+{
+	sprite = iceFloor;
+}
+void IceFloor::draw(SDL_Surface *drawTo, int xTile, int yTile, int xOff, int yOff){
+	int xStart = xTile*TILE_SIZE + xInitial + xOff;
+	int yStart = yTile*TILE_SIZE + yInitial + yOff;
+	apply_surface(xStart, yStart, tiles, drawTo);
+	apply_surface(xStart, yStart, sprite, drawTo);
+}
+void IceFloor::onEnter(Object *other)
+{
+	other->startMove(other->prevMove);
+}
+bool IceFloor::isSolid()
+{
+	return false;
+}
